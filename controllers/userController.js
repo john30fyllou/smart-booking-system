f72bc3ae1f4dd6e6adcd12fcf5f940ce55dbd8c6
Iwannis Triantafyllou <Iwannis Triantafyllou>
2026-08-19 16:18:32 +0300

    Add JWT authentication and protected routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const getAllUsers = (req, res) => {
     const sql = 'SELECT id, first_name, last_name, email, role, created_at FROM users';
@@ -60,7 +61,72 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: 'Email and password are required'
+        });
+    }
+
+    const sql = 'SELECT * FROM users WHERE email = ?';
+
+    db.query(sql, [email], async (err, results) => {
+        if (err) {
+            console.error('Login database error:', err);
+
+            return res.status(500).json({
+                message: 'Database error'
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(401).json({
+                message: 'Invalid email or password'
+            });
+        }
+
+        const user = results[0];
+
+        const passwordMatches = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!passwordMatches) {
+            return res.status(401).json({
+                message: 'Invalid email or password'
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '1h'
+            }
+        );
+
+        res.status(200).json({
+            message: 'Login successful',
+            token: token,
+            user: {
+                id: user.id,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email,
+                role: user.role
+            }
+        });
+    });
+};
+
 module.exports = {
     getAllUsers,
-    registerUser
+    registerUser,
+    loginUser
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
 const router = express.Router();
+const authenticateToken = require('../middleware/authMiddleware');
 
 const { getAllUsers, 
-        registerUser 
+        registerUser,
+        loginUser 
 } = require('../controllers/userController');
 
 router.get('/', getAllUsers);
 router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', authenticateToken,(req, res) =>{
+    res.status(200).json({
+        message: 'Protected route accessed successfully',
+        user: req.user
+    });
+});
 
 module.exports = router;
